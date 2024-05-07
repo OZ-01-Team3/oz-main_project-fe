3f69b14c2c3e5e737fee9a6140bbdd0d9ae4c24a
hayeon <hayeon010904@gmail.com>
2024-05-07 18:54:21 +0900

    style: 최초 로그인 시 모달 스타일링, global 모달 배경 색 추가, icon추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Banner from "@/components/Banner";
 import Event from "@/components/Event";
 import Footer from "@/components/Footer";
 import Products from "@/components/Products";
+import StyleModal from "@/components/StyleModal";
 
 const MainPage = () => {
   return (
     <div>
+      <StyleModal />
       <Banner />
       <div className="pt-32 w-2/3 ml-auto mr-auto ">
         <p className="text-2xl mb-4">내가 선택한 스타일에 맞는 추천스타일</p>
