409dc22d5e8c7227902d7dfaedf9a71d3c61f5c0
hayeon <hayeon010904@gmail.com>
2024-05-07 11:04:46 +0900

    style: eslint,prettier삭제, 정렬수정,루트페이지 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
+import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
+import Products from "@/components/Products";
 
-export default function Home() {
+const MainPage = () => {
   return (
     <div>
-      메인입기다
+      <Banner />
+      <div className="pt-32 w-2/3 ml-auto mr-auto ">
+        <p className="text-2xl mb-4">내가 선택한 스타일에 맞는 추천스타일</p>
+        <Products />
+        <hr className=" w-3/4 ml-auto mr-auto mt-10 mb-20 text-mainWhite " />
+        <p className="text-2xl mb-4 font-didot">NEW</p>
+        <Products />
+        <hr className=" w-3/4 ml-auto mr-auto mt-10 mb-20 text-mainWhite " />
+        <p className="text-2xl mb-4 font-didot">EVENT</p>
+        <div className="w-full grid  grid-cols-2 h-60  gap-x-3  ">
+          <div className="bg-gray-300  "></div>
+          <div className="bg-gray-300 "></div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
-}
+};
+
+export default MainPage;
--- productData.tsx
+++ productData.tsx
@@ -1,77 +1,68 @@
+const products = [
+  {
+    id: 1,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240117/3800972/3800972_17071843073582_500.jpg",
+    title: "옷정리합니다",
+    description: "가방 예뿌네잉",
+    price: 43200,
+  },
+  {
+    id: 2,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240102/3771106/3771106_17041841891976_320.jpg",
+    title: "대충 신발신발",
+    description: "신발사세요 예뿌네잉",
+    price: 43200,
+  },
+  {
+    id: 3,
+    image:
+      "https://image.msscdn.net/images/goods_img/20230412/3228764/3228764_17141243417273_320.jpg",
+    title: "대충 바지",
+    description: "다리짧아보이는 바지",
+    price: 43200,
+  },
+  {
+    id: 4,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240226/3901296/3901296_17098728376352_320.jpg",
+    title: "대충 치마",
+    description: "똥꼬치마",
+    price: 43200,
+  },
+  {
+    id: 5,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240415/4054553/4054553_17134275558872_125.jpg",
+    title: "세인트 피그먼트 반팔티",
+    description: "안티티티티 프레즐먹고싶다",
+    price: 43200,
+  },
+  {
+    id: 6,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240117/3800972/3800972_17071843073582_500.jpg",
+    title: "옷정리합니다",
+    description: "가방 예뿌네잉",
+    price: 43200,
+  },
+  {
+    id: 7,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240102/3771106/3771106_17041841891976_320.jpg",
+    title: "대충 신발신발",
+    description: "신발사세요 예뿌네잉",
+    price: 43200,
+  },
+  {
+    id: 8,
+    image:
+      "https://image.msscdn.net/images/goods_img/20230412/3228764/3228764_17141243417273_320.jpg",
+    title: "대충 바지",
+    description: "다리짧아보이는 바지",
+    price: 43200,
+  },
+];
 
- const products= [
-{
-    id:1,
-    image:'https://image.msscdn.net/images/goods_img/20240117/3800972/3800972_17071843073582_500.jpg',
-    title: '옷정리합니다',
-    description:'가방 예뿌네잉',
-    price: 43200
-},
-{
-    id:2,
-    image:'https://image.msscdn.net/images/goods_img/20240102/3771106/3771106_17041841891976_320.jpg',
-    title: '대충 신발신발',
-    description:'신발사세요 예뿌네잉',
-    price:43200
-},
-{
-    id:3,
-    image:'https://image.msscdn.net/images/goods_img/20230412/3228764/3228764_17141243417273_320.jpg',
-    title: '대충 바지',
-    description:'다리짧아보이는 바지',
-    price: 43200
-},
-{
-    id:4,
-    image:'https://image.msscdn.net/images/goods_img/20240226/3901296/3901296_17098728376352_320.jpg',
-    title: '대충 치마',
-    description:'똥꼬치마',
-    price: 43200
-},
-{
-    id:5,
-    image:'https://image.msscdn.net/images/goods_img/20240415/4054553/4054553_17134275558872_125.jpg',
-    title: '세인트 피그먼트 반팔티',
-    description:'안티티티티 프레즐먹고싶다',
-    price: 43200
-    }
-,{
-    id:6,
-    image:'https://image.msscdn.net/images/goods_img/20240117/3800972/3800972_17071843073582_500.jpg',
-    title: '옷정리합니다',
-    description:'가방 예뿌네잉',
-    price: 43200
-},
-{
-    id:7,
-    image:'https://image.msscdn.net/images/goods_img/20240102/3771106/3771106_17041841891976_320.jpg',
-    title: '대충 신발신발',
-    description:'신발사세요 예뿌네잉',
-    price:43200
-},
-{
-    id:8,
-    image:'https://image.msscdn.net/images/goods_img/20230412/3228764/3228764_17141243417273_320.jpg',
-    title: '대충 바지',
-    description:'다리짧아보이는 바지',
-    price: 43200
-},
-// {
-//     id:9,
-//     image:'https://image.msscdn.net/images/goods_img/20240226/3901296/3901296_17098728376352_320.jpg',
-//     title: '대충 치마',
-//     description:'똥꼬치마',
-//     price: 43200
-// },
-// {
-//     id:10,
-//     image:'https://image.msscdn.net/images/goods_img/20240415/4054553/4054553_17134275558872_125.jpg',
-//     title: '세인트 피그먼트 반팔티',
-//     description:'안티티티티 프레즐먹고싶다',
-//     price: 43200
-//     }
-    ]
-
-
-export default products
-
+export default products;
