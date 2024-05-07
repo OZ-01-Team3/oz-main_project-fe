d0e9a2be71ad947eace7fb94dfb98ad8aa66aa7c
deswaq1220 <deswaq789@naver.com>
2024-05-08 01:32:37 +0900

    Chore: 더미데이터 수정

--- orderProductData.ts
+++ orderProductData.ts
@@ -10,8 +10,8 @@ const orderProducts = [
   {
     id: 2,
     image:
-      "https://image.msscdn.net/images/goods_img/20240102/3771106/3771106_17041841891976_320.jpg",
-    title: "대충 신발신발",
+      "https://image.msscdn.net/images/goods_img/20240216/3877002/3877002_17085052458926_500.jpg",
+    title: "대충 셔츠",
     rental: "2024.05.01",
     return: "2024.05.08",
   },
@@ -58,8 +58,8 @@ const orderProducts = [
   {
     id: 8,
     image:
-      "https://image.msscdn.net/images/goods_img/20230412/3228764/3228764_17141243417273_320.jpg",
-    title: "대충 바지",
+      "https://image.msscdn.net/images/goods_img/20231004/3603458/3603458_16965720333230_500.jpg",
+    title: "대충 가방",
     rental: "2024.05.01",
     return: "2024.05.08",
   },
