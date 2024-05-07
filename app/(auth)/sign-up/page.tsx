907fc503425dd07fa4d04262646bb3defabdc023
deswaq1220 <deswaq789@naver.com>
2024-05-07 14:33:54 +0900

    Style: 회원가입 스타일

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,61 @@
+"use client"
 
+import AuthInput from "@/components/AuthInput";
+import CommonButton from "@/components/CommonButton";
+import { useRef } from "react";
 const signUp = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <div>signUp</div>
+    <div className="w-full  bg-mainBlack flex justify-center items-center flex-col">
+      <div className="w-[460px] flex justify-center flex-col mt-40">
+        <p className="font-didot text-3xl text-center mb-7">Sign Up</p>
+        <form>
+          <div className="w-full flex items-center">
+            <div className="flex-grow">
+              <AuthInput ref={inputRef}>Email</AuthInput>
+            </div>
+            <div className="flex-none w-1/4 ml-2">
+              <CommonButton className="w-full h-12 rounded-lg bg-mainWhite px-3.5 py-2.5 mt-6 text-base font-base text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">인증번호 발송</CommonButton>
+            </div>
+          </div>
+
+          <div className="w-full flex items-center">
+            <div className="flex-grow relative">
+              <AuthInput ref={inputRef}>Auth Code</AuthInput>
+
+              <p className=" absolute bottom-3.5 right-3 text-subGray text-sm">3:00</p>
+
+            </div>
+            <div className="flex-none w-1/4 ml-2">
+              <CommonButton className="w-full h-12 rounded-lg bg-mainWhite px-3.5 py-2.5 mt-6 text-base font-base text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">확인</CommonButton>
+            </div>
+          </div>
+          <AuthInput>Password</AuthInput>
+          <AuthInput>Confirm Password</AuthInput>
+          <AuthInput>Nickname</AuthInput>
+          <AuthInput>Nickname</AuthInput>
+          <AuthInput>Phone</AuthInput>
+          <CommonButton className=" w-full rounded-lg bg-mainWhite px-3.5 py-2.5 text-lg font-semibold font-didot text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-6">Sign In</CommonButton>
+        </form>
+      </div>
+      <div className="mt-6 font-light">회원이신가요?
+        <button type="button" className="font-semibold ml-2">
+          Sign in
+        </button>
+      </div>
+      {/* <div className="flex justify-center items-center mt-10 w-[460px]">
+        <span className=" w-full h-[1px] bg-mainWhite mr-2"></span>
+        <p>or</p>
+        <span className=" w-full h-[1px] bg-mainWhite ml-2"></span>
+      </div>
+      <div className="flex flex-row mt-5">
+        <img src="/images/naver.png" className="w-[39px] h-[39px] mr-3 cursor-pointer" />
+        <div className=" w-[39px] h-[39px] rounded-full overflow-hidden mr-3 cursor-pointer" cursor-pointer>
+          <img src="/images/kakao.png" className="w-full h-full object-cover " />
+        </div>
+        <img src="/images/google.svg" className="cursor-pointer" />
+      </div> */}
+    </div>
   )
 }
 
