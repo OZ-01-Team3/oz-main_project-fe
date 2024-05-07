1b48cd588e2baacec66cae82c461cce87c3aecc2
deswaq1220 <deswaq789@naver.com>
2024-05-08 01:52:54 +0900

    Refector: sidenav 하드코딩 수정

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,19 +1,34 @@
 'use client'
 
-const SideBar = () => {
+
+type SidebarItemProps = {
+  text: string;
+};
+
+
+const SidebarItem = ({ text }: SidebarItemProps) => {
   return (
-    <nav className=" w-80 h-auto h-min-[300px] border-r-[1px] border-mainWhite p-5">
+    <li className="p-2 cursor-pointer">{text}</li>
+  );
+};
 
+const SideBar = () => {
+  const sidebarItems = [
+    { text: '내 옷장' },
+    { text: '주문 이력' },
+    { text: '대여 상품' },
+    { text: '회원 정보' },
+  ];
+
+  return (
+    <nav className="w-80 h-auto h-min-[300px] border-r-[1px] border-mainWhite p-5">
       <ul className="flex justify-end items-end flex-col text-xl">
-        <li className="p-2 cursor-pointer">내 옷장</li>
-        <li className="p-2 cursor-pointer">주문 이력</li>
-        <li className="p-2 cursor-pointer">대여 상품</li>
-        <li className="p-2 cursor-pointer">회원 정보</li>
+        {sidebarItems.map((item, index) => (
+          <SidebarItem key={index} text={item.text} />
+        ))}
       </ul>
-
-
     </nav>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
