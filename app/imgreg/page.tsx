60cbbf22e173cba23bd4054c6547363d5e1dec33
rnjsdydwp9 <rnjsdydwp9@gmail.com>
2024-05-08 12:00:05 +0900

    Chore: 이미지수정

---
+++ app/imgreg/page.tsx
@@ -0,0 +1,13 @@
+import Detailimg from '@/components/Detailimg'
+import React from 'react'
+
+const imgReg = () => {
+  return (
+    <div className=' w'>
+    <Detailimg />
+
+    </div>
+  )
+}
+
+export default imgReg
