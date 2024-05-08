b138367fadec1c4626d822564eac52fce0d89f80
deswaq1220 <deswaq789@naver.com>
2024-05-08 13:54:00 +0900

    Refactor: 로그인 하드코딩 수정

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,6 +3,22 @@
 import AuthInput from "@/components/AuthInput";
 import CommonButton from "@/components/CommonButton";
 import { useRef } from "react";
+
+//로그인 폼 입력 필드
+const formData: { id: string; type: "text" | "number" | "password" | "email" | "tel"; placeholder: string; label: string; }[] = [
+  { id: "email", type: "email", placeholder: "이메일", label: "Email" },
+  { id: "password", type: "password", placeholder: "비밀번호", label: "Password" },
+];
+
+// 소셜미디어 로그인 버튼
+const socialMedia = [
+  { name: "naver", src: "/images/naver.png" },
+  { name: "kakao", src: "/images/kakao.png" },
+  { name: "google", src: "/images/google.svg" },
+];
+
+
+
 const signIn = () => {
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -14,9 +30,12 @@ const signIn = () => {
       <div className="w-[460px] flex justify-center flex-col mt-40">
         <p className="font-didot text-3xl text-center mb-7">Sign In</p>
         <form>
-          <AuthInput ref={inputRef} >Email</AuthInput>
-          <AuthInput>Password</AuthInput>
-          <CommonButton className=" w-full rounded-lg bg-mainWhite px-3.5 py-2.5 text-lg font-semibold font-didot text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-6">Sign In</CommonButton>
+          {formData.map(({ id, type, placeholder, label }, index) => (
+            <AuthInput key={id} ref={index === 0 ? inputRef : null} type={type} placeholder={placeholder}>
+              {label}
+            </AuthInput>
+          ))}
+          <CommonButton className="w-full rounded-lg bg-mainWhite px-3.5 py-2.5 text-lg font-semibold font-didot text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-6">Sign In</CommonButton>
         </form>
       </div>
       <div className="mt-6 font-light">아직 회원이 아니신가요?
@@ -30,11 +49,11 @@ const signIn = () => {
         <span className=" w-full h-[1px] bg-mainWhite ml-2"></span>
       </div>
       <div className="flex flex-row mt-5">
-        <img src="/images/naver.png" className="w-[39px] h-[39px] mr-3 cursor-pointer" />
-        <div className=" w-[39px] h-[39px] rounded-full overflow-hidden mr-3 cursor-pointer" cursor-pointer>
-          <img src="/images/kakao.png" className="w-full h-full object-cover " />
-        </div>
-        <img src="/images/google.svg" className="cursor-pointer" />
+        {socialMedia.map(({ name, src }) => (
+          <div key={name} className="w-[39px] h-[39px] mr-3 cursor-pointer overflow-hidden rounded-full">
+            <img src={src} alt={name} className="w-full h-full object-cover" />
+          </div>
+        ))}
       </div>
     </div>
   )
