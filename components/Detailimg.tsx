374270508c5fcbd4073646577a8bd66c6bdcd90d
rnjsdydwp9 <rnjsdydwp9@gmail.com>
2024-05-09 00:23:11 +0900

    Style: 이미지수정

--- components/Detailimg.tsx
+++ components/Detailimg.tsx
@@ -4,23 +4,27 @@ import React from 'react';
 const Detailimg = () => {
     return (
         <div className="col-span-full">
-        <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-          Cover photo
-        </label>
-        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+
+          <p className="text-center text-x leading-10 text-mainWhite py-24" >상품 이미지 등록</p>
+      
+        <div className="mt-5 flex justify-center rounded-lg border border-dashed border-mainWhite px-6 py-10">
           <div className="text-center">
-            <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-            <div className="mt-4 flex text-sm leading-6 text-gray-600">
+            <PhotoIcon className="mx-auto h-52 w-52 text-gray-300" aria-hidden="true" />
+       
+          
+            
+            <div className="mt-4 flex text-sm leading-6 text-mainWhite">
               <label
                 htmlFor="file-upload"
                 className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
               >
-                <span>Upload a file</span>
+                <span>파일 업로드</span>
                 <input id="file-upload" name="file-upload" type="file" className="sr-only" />
               </label>
               <p className="pl-1">or drag and drop</p>
             </div>
-            <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+           <p className="text-xs leading-5 text-mainWhite">클릭하거나 파일을 여기에 드롭하여 선택하세요</p>
+                 
           </div>
         </div>
       </div>
