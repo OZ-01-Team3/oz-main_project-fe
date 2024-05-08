8f8b9bbd02f1a989a356c412317ea270b71335d2
rnjsdydwp9 <rnjsdydwp9@gmail.com>
2024-05-08 11:25:03 +0900

    chore: 디테일 이미지수정

--- components/Detailimg.tsx
+++ components/Detailimg.tsx
@@ -1,13 +1,29 @@
+import { PhotoIcon } from '@heroicons/react/16/solid';
 import React from 'react';
 
 const Detailimg = () => {
     return (
-        <form>
-            <div className="yourClassName">
-                <label htmlFor="file">사진첨부</label>
-                <input className="hidden" type="file" multiple accept="image/*" />
+        <div className="col-span-full">
+        <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+          Cover photo
+        </label>
+        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+          <div className="text-center">
+            <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+            <div className="mt-4 flex text-sm leading-6 text-gray-600">
+              <label
+                htmlFor="file-upload"
+                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+              >
+                <span>Upload a file</span>
+                <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+              </label>
+              <p className="pl-1">or drag and drop</p>
             </div>
-        </form>
+            <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+          </div>
+        </div>
+      </div>
     );
 }
 
