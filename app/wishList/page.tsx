7770764099a14b8c28305eeb8a7f92cad5cb5750
hayeon <hayeon010904@gmail.com>
2024-05-08 11:51:12 +0900

    style: wishLIst css

---
+++ app/wishList/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import ProductDetailModal from "@/components/ProductDetailModal";
+import Products from "@/components/Products";
+import { useState } from "react";
+
+const WishListPage = () => {
+  const [detailModalOpen, setDetailModalOpen] = useState(false);
+  return (
+    <>
+      <ProductDetailModal
+        detailModalOpen={detailModalOpen}
+        setDetailModalOpen={setDetailModalOpen}
+      />
+      <div className="pt-20 w-2/3 ml-auto mr-auto ">
+        <h1 className="text-4xl mb-20 font-didot text-center">Wish List</h1>
+        <Products setDetailModalOpen={setDetailModalOpen} />
+      </div>
+    </>
+  );
+};
+
+export default WishListPage;
