2840a5ac17ba906ceef73b95452f193f560e9cbe
hayeon <hayeon010904@gmail.com>
2024-05-08 14:45:02 +0900

    refactor: tailwind Icon import 로 변경

--- components/ProductDetail/ProductDetailModal.tsx
+++ components/ProductDetail/ProductDetailModal.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Dispatch, SetStateAction, useRef } from "react";
 import ModalStyleButton from "./ModalStyleButton";
 import ProductDetailsDescription from "./ProductDetailsDescription";
@@ -36,21 +37,11 @@ const ProductDetailModal = ({
         {/*모달 */}
         <div className="flex flex-row justify-center pl-10 pr-10 bg-mainWhite h-3/4 w-8/12 pt-10 pb-5 rounded-lg relative">
           {/* 닫힘버튼 */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
+          <XMarkIcon
             className="w-6 h-6 text-mainBlack absolute right-5 top-5 cursor-pointer"
             onClick={handleCloseModal}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18 18 6M6 6l12 12"
-            />
-          </svg>
+          />
+
           {/* 사진영역 */}
           <div className="w-1/2 pr-10 pb-20 flex flex-col">
             <img
--- components/Products.tsx
+++ components/Products.tsx
@@ -1,5 +1,6 @@
 "use client";
 import products from "@/productData";
+import { HeartIcon } from "@heroicons/react/24/outline";
 import { Dispatch, SetStateAction, useRef } from "react";
 interface ProductsProps {
   setDetailModalOpen: Dispatch<SetStateAction<boolean>>;
@@ -23,21 +24,7 @@ const Products = ({ setDetailModalOpen }: ProductsProps) => {
             <img src={product.image} className="aspect-[3/3.5] relative mb-2" />
             <div className="flex justify-between">
               <p className="font-bold text-base">{product.title}</p>
-              {/* 엑스버튼  */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                />
-              </svg>
+              <HeartIcon className="w-6 h-6" />
             </div>
             <p className="text-sm">{product.description}</p>
             <p className="text-sm">{product.price.toLocaleString()}</p>
