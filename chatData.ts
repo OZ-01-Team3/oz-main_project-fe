90291ef90afd7a98a885813b8fe0f25e2d7ccea9
deswaq1220 <deswaq789@naver.com>
2024-05-08 22:45:21 +0900

    style:채팅 알림바

---
+++ chatData.ts
@@ -0,0 +1,68 @@
+const chatData = [
+  {
+    id: 1,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240117/3800972/3800972_17071843073582_500.jpg",
+    name: "빵용",
+    content: "안녕하세요? 대여비 깎깎깎깎깎ㄲ아주시술?",
+    time: "1분전",
+  },
+  {
+    id: 2,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240216/3877002/3877002_17085052458926_500.jpg",
+    name: "하염빵",
+    content: "모자이미 대여됐나요?",
+    time: "10분전",
+  },
+  {
+    id: 3,
+    image:
+      "https://image.msscdn.net/images/goods_img/20230412/3228764/3228764_17141243417273_320.jpg",
+    name: "매콤시티 매콤주먹",
+    content: "19일 4시에 가능하신가요?",
+    time: "1시간전",
+  },
+  {
+    id: 4,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240226/3901296/3901296_17098728376352_320.jpg",
+    name: "예쏘라하~",
+    content: "이거 저 주세요",
+    time: "1시간전",
+  },
+  {
+    id: 5,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240415/4054553/4054553_17134275558872_125.jpg",
+    name: "똥쟁이유경록",
+    content: "뿡",
+    time: "1일전",
+  },
+  {
+    id: 6,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240117/3800972/3800972_17071843073582_500.jpg",
+    name: "섹시다이너마이트",
+    content: "제발 저 대여하게해주세요제발요",
+    time: "1일전",
+  },
+  {
+    id: 7,
+    image:
+      "https://image.msscdn.net/images/goods_img/20240102/3771106/3771106_17041841891976_320.jpg",
+    name: "박박디라라",
+    content: "옷 맞교환 하실래요?",
+    time: "2일전",
+  },
+  {
+    id: 8,
+    image:
+      "https://image.msscdn.net/images/goods_img/20231004/3603458/3603458_16965720333230_500.jpg",
+    name: "김형진",
+    content: "대머리 깎아라",
+    time: "3일전",
+  },
+];
+
+export default chatData;
