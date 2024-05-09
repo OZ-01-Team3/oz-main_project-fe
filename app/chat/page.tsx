8d93ae7945a8f6ef32045b1a131386b609a0adb4
hayeon <hayeon010904@gmail.com>
2024-05-09 22:27:55 +0900

    style: chat css 수정

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,16 +1,14 @@
-import Chat from "@/components/Chat/Chat";
-import ChatLists from "@/components/Chat/ChatLists";
+import Chat from '@/components/Chat/Chat';
+import ChatLists from '@/components/Chat/ChatLists';
 const chat = [];
 const page = () => {
   return (
     <div className="sm:w-full h-[calc(100vh-99.59px)] bg-white text-mainBlack xl:px-32 sm:px-10 md:px-10 lg:px-10 ">
-      <div className="text-2xl pl-6 h-14 flex items-center font-semibold sm:justify-center md:justify-start">
-        채팅
-      </div>
+      <div className="text-2xl pl-6 h-14 flex items-center font-semibold sm:justify-center md:justify-start">채팅</div>
       {/* 반반 나누는ㄴ 레이아웃 */}
       <div className="flex  w-full h-[calc(100%-70px)] md:flex-row 2xl:flex-row ">
         {/* 채팅목록 */}
-        <div className="w-2/5 pl-3 pr-5 border-r-[1px] border-gray overflow-y-scroll scrollbar-hide sm:w-20  ">
+        <div className="w-2/5 pl-3 pr-5 border-r-[1px] border-gray overflow-y-scroll scrollbar-hide sm:w-28 sm:px-2   ">
           <ChatLists />
         </div>
         {/* 채팅방 */}
--- components/Chat/ChatLIst.tsx
+++ components/Chat/ChatLIst.tsx
@@ -5,34 +5,26 @@ interface ChatListProps {
   product: string;
   profile?: string;
 }
-const UserResponseCss =
-  "flex flex-col items-start justify-center w-72 pr-1  h-18 md:mr-0 ml-1 md:h-20";
-const ProductResCss = "w-16 h-16 aspect-[1/1] border-gray rounded-md border ";
+const UserResponseCss = 'flex flex-col items-start justify-center w-72 pr-1  h-18 md:mr-0 ml-1 md:h-20 pt-2';
+const ProductResCss = 'w-16 h-16 aspect-[1/1] border-gray rounded-md border ';
 
 const ChatLIst = ({ user, content, time, profile, product }: ChatListProps) => {
   return (
     <>
-      <div className="flex flex-row items-center justify-center  px-2 cursor-pointer  flex-none  md:px-0 ">
+      <div className="flex flex-row items-center justify-center  px-2 cursor-pointer  flex-none  md:px-0 sm:pt-2">
         <div className="w-14 h-14 aspect-[1/1] border-mainBlack rounded-full border md:w-12 md:h-12 mr-2">
-          <img
-            src={profile}
-            className="w-full h-full  object-cover rounded-full"
-            alt="프로필 이미지"
-          />
+          <img src={profile} className="w-full h-full  object-cover rounded-full" alt="프로필 이미지" />
         </div>
         <div className={`sm:hidden md:${UserResponseCss}  `}>
           <p className="text-sm h-5 overflow-hidden  font-semibold ">{user}</p>
 
-          <p className="text-[12px] font-light overflow-hidden h-11 md:h-9 sm:text-xs">
+          <p className="text-[12px] font-light overflow-hidden h-10 md:h-8 sm:text-xs">
             {content.length > 30 ? `${content.substring(0, 30)}...` : content}
           </p>
           <p className="text-xs font-thin">{time}</p>
         </div>
         <div className={`xl:${ProductResCss} lg:hidden md:hidden sm:hidden`}>
-          <img
-            src={product}
-            className="w-full h-full object-cover  rounded-md"
-          />
+          <img src={product} className="w-full h-full object-cover  rounded-md" />
         </div>
       </div>
       <div className="flex-1 border-b mx-auto mt-2 border-hrGray"></div>
