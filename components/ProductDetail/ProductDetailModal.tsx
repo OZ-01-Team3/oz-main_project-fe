05308233db2aebe1a62e8731e2e2fdf2696304ea
hayeon <hayeon010904@gmail.com>
2024-05-10 03:49:18 +0900

    style: productDetailModal 반응형 css 수정

--- components/ProductDetail/ProductDetailModal.tsx
+++ components/ProductDetail/ProductDetailModal.tsx
@@ -2,6 +2,7 @@
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { Dispatch, SetStateAction, useRef } from 'react';
 import ModalStyleButton from './ModalStyleButton';
+import ProductDetailResponse from './ProductDetailResponse';
 import ProductDetailsDescription from './ProductDetailsDescription';
 
 interface PropsType {
@@ -18,48 +19,49 @@ const ProductDetailModal = ({ detailModalOpen, setDetailModalOpen }: PropsType) 
   const handleCloseModal = () => {
     setDetailModalOpen(false);
   };
-
   return (
     detailModalOpen && (
-      // 바깥영역
-      <div
-        className="flex w-full h-screen fixed inset-0 z-50 bg-modalBg justify-center items-center"
-        ref={outerBoxRef}
-        onClick={e => {
-          if (e.target === outerBoxRef.current) {
-            handleCloseModal();
-          }
-        }}
-      >
-        {/*모달 */}
-
-        <div className="flex flex-row justify-center pl-10 pr-10 bg-mainWhite h-[565.5px] w-[950px] lg:w-[730px] md:w-[620px] sm:w-full sm:h-full sm:flex-col pt-10 pb-5 rounded-lg relative  lg:h-[500px] md:h-[400px] sm:overflow-y-scroll sm:justify-start sm:items-center sm:rounded-none">
-          {/* 닫힘버튼 */}
-          <XMarkIcon
-            className="w-6 h-6 text-mainBlack absolute right-5 top-5 cursor-pointer"
-            onClick={handleCloseModal}
-          />
-
-          {/* 사진영역 */}
-          <div className="w-1/2 pr-10 flex flex-col sm:w-full sm:pr-0 sm:justify-center sm:items-center">
-            <img
-              src="https://image.msscdn.net/images/goods_img/20240117/3800972/3800972_17071843073582_500.jpg"
-              className="w-96 h-[410px]"
+      <>
+        <div
+          className="flex w-full h-screen fixed inset-0 z-50 bg-modalBg justify-center items-center"
+          ref={outerBoxRef}
+          onClick={e => {
+            if (e.target === outerBoxRef.current) {
+              handleCloseModal();
+            }
+          }}
+        >
+          <ProductDetailResponse setDetailModalOpen={setDetailModalOpen} />
+          {/* 모달 컨텐츠 */}
+          <div className="flex flex-row justify-center pl-10 pr-10 bg-mainWhite h-[565.5px] w-[950px] lg:w-[730px] md:w-[620px] sm:w-full sm:h-full sm:flex-col pt-10 pb-5 rounded-lg relative lg:h-[500px] md:h-[400px] sm:overflow-y-scroll sm:justify-start sm:items-center sm:hidden md:hidden">
+            {/* 닫기 버튼 */}
+            <XMarkIcon
+              className="w-6 h-6 text-mainBlack absolute right-5 top-5 cursor-pointer"
+              onClick={handleCloseModal}
             />
-            <div className="flex flex-row justify-between text-mainBlack mt-2 mb-2 pl-2 pr-2">
-              <div className="text-lg font-semibold">대여비</div>
-              <div className="text-base ">8,000(1일)</div>
+            {/* 사진 영역 */}
+            <div className="w-1/2 pr-10 flex flex-col sm:w-full sm:pr-0 sm:justify-center sm:items-center">
+              <img
+                src="https://image.msscdn.net/images/goods_img/20240117/3800972/3800972_17071843073582_500.jpg"
+                className="w-96 h-[410px]"
+                alt="상품 이미지"
+              />
+              <div className="flex flex-row justify-between text-mainBlack mt-2 mb-2 pl-2 pr-2">
+                <div className="text-lg font-semibold">대여비</div>
+                <div className="text-base">8,000(1일)</div>
+              </div>
+              <div className="pl-2 pr-2">
+                {/* 스타일 버튼들 */}
+                {style.map(product => (
+                  <ModalStyleButton key={product}>{product}</ModalStyleButton>
+                ))}
+              </div>
             </div>
-            <div className="pl-2 pr-2">
-              {style.map(product => (
-                <ModalStyleButton>{product}</ModalStyleButton>
-              ))}
-            </div>
+            {/* 상세 설명 영역 */}
+            <ProductDetailsDescription />
           </div>
-          {/* text영역 */}
-          <ProductDetailsDescription />
         </div>
-      </div>
+      </>
     )
   );
 };
