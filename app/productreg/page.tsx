3f283d7a16b4dc18624216dd11dd7c2829149bde
rnjsdydwp9 <rnjsdydwp9@gmail.com>
2024-05-10 05:10:06 +0900

    Style: 상품상세등록

--- app/productreg/page.tsx
+++ app/productreg/page.tsx
@@ -1,26 +1,21 @@
 
 import ProductReg from '@/components/ProductReg';
-import CommonButton from "@/components/CommonButton";
+
 
 
 const productReg = () => {
 
 
-      return (
-        <form>
-        <div className='text-center'>
+  return (
+    <div className="px-4 sm:px-6 lg:px-8"> 
+      <div className="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8"> 
+        <div className="text-center">
           <ProductReg />
-
-          <div className='text-center'>
-          <CommonButton className=" w-44 rounded-lg bg-mainBlack px-3.5 py-2.5 text-lg font-semibold font-didot  text-mainWhite  shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-10 ml-3 border border-solid border-mainWhite">임시저장</CommonButton>
-          <CommonButton className=" w-44 rounded-lg bg-mainWhite px-3.5 py-2.5 text-lg font-semibold font-didot text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-10 ml-3 border border-solid border-mainWhite">등록하기</CommonButton>
-          
-        
-          </div>
-         </div>
-        </form>
-      );
-    }
+        </div>
+      </div>
+    </div>
+  );
+};
 
 
 export default productReg
