c7df4a25b188cd9a70cb367d1b492058ed41a6a1
rnjsdydwp9 <rnjsdydwp9@gmail.com>
2024-05-10 01:08:18 +0900

    Style: 상품등록

--- app/productreg/page.tsx
+++ app/productreg/page.tsx
@@ -9,19 +9,15 @@ const productReg = () => {
       return (
         <form>
         <div className='text-center'>
-        
-
-         
           <ProductReg />
 
-
-          <div className='text-right'>
+          <div className='text-center'>
           <CommonButton className=" w-44 rounded-lg bg-mainBlack px-3.5 py-2.5 text-lg font-semibold font-didot  text-mainWhite  shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-10 ml-3 border border-solid border-mainWhite">임시저장</CommonButton>
           <CommonButton className=" w-44 rounded-lg bg-mainWhite px-3.5 py-2.5 text-lg font-semibold font-didot text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-10 ml-3 border border-solid border-mainWhite">등록하기</CommonButton>
           
         
-        </div>
-        </div>
+          </div>
+         </div>
         </form>
       );
     }
--- components/ProductReg.tsx
+++ components/ProductReg.tsx
@@ -7,37 +7,38 @@ const ProductReg = () => {
     <form className="  px-36">
         <br></br>
         <p className="text-left text-3xl ">상품 정보</p>
-        <hr className=" w-12/12 ml-auto mr-auto mt-10 mb-20 text-mainWhite " />
+        <hr className=" w-12/12 ml-auto mr-auto mt-6 mb-7 text-mainWhite " />
     <div>
       <div className="mb-4">
         <div className="flex items-center">
           <span className=" w-48 flex-shrink-0 text-left mr-1">상품명 </span>
           <input
-            className="shadow appearance-none border rounded  w-7/12 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+            className="shadow appearance-none border rounded  w-7/12 py-2 px-4 text-subGray leading-tight focus:outline-none focus:shadow-outline "
             type="text"
             placeholder="상품명을 입력하세요"
           />
-          <span className="text-sm text-white ml-12 mt-2">0/40</span>
+          <span className="text-sm text-white ml-3">0/40</span>
         </div>
       </div>
+      <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
       
       <div className="mb-4">
         <div className="flex items-center">
-          <span className=" w-48  flex-shrink-0 text-left mr-1">대여비 </span>
+          <span className=" w-48  flex-shrink-0 text-left mr-1">대여비</span>
           <input
-            className="shadow appearance-none border rounded w-7/12 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-7/12 py-2 px-4 text-subGray leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             placeholder="대여비를 입력하세요"
           />
-          <span className=" text-sm text-white ml-16 mt-2">원</span>
+          <span className=" text-sm text-white ml-3">원</span>
         </div>
       </div>
-
+      <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
       <div className="mb-4">
         <div className="flex items-center">
           <span className="w-48 flex-shrink-0 text-left mr-1">사이즈</span>
           <select
-            className="shadow appearance-none border rounded w-8/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-1"
+            className="shadow appearance-none border rounded w-8/12 py-2 px-3 text-subGray leading-tight focus:outline-none focus:shadow-outline mt-1"
           >
             <option value="">S,M,L</option>
             <option value="S">S</option>
@@ -47,23 +48,23 @@ const ProductReg = () => {
           </select>
           </div>
         </div>
-
+        <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
         <div className="mb-4">
         <div className="flex items-center">
           <span className=" w-48  flex-shrink-0 text-left mr-1">브랜드 </span>
           <input
-            className="shadow appearance-none border rounded w-8/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-8/12 py-2 px-3 text-subGray leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             placeholder="브랜드"
           />
         </div>
       </div>
-
+      <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
         <div className="mb-4">
         <div className="flex items-center">
           <span className="w-48 flex-shrink-0 text-left mr-1">카테고리</span>
           <select
-            className="shadow appearance-none border rounded w-8/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-1"
+            className="shadow appearance-none border rounded w-8/12 py-2 px-3 text-subGray leading-tight focus:outline-none focus:shadow-outline mt-1"
           >
             <option value="">전체</option>
             <option value="아우터">아우터</option>
@@ -74,35 +75,37 @@ const ProductReg = () => {
           </select>
           </div>
         </div>
+        <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
+
       <div className="mb-4">
         <div className="flex items-center">
           <span className="w-48 flex-shrink-0 text-left mr-1">구매시기 </span>
           <input
-            className="shadow appearance-none border rounded w-8/12 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-8/12 py-2 px-4 text-subGray leading-tight focus:outline-none focus:shadow-outline"
             type="date"
           />
       </div>
     </div>
-
+    <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
     
 
 
     <div className="mb-4">
         <div className="flex items-center">
-          <span className=" w-48  flex-shrink-0 text-left mr-1">설명 </span>
+          <span className=" w-48  flex-shrink-0 text-left   mr-1">설명 </span>
           <input
-            className="text-left shadow appearance-none border rounded w-8/12 py-20 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className=" text-left shadow appearance-none border rounded w-8/12 py-20 px-3 text-subGray leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             placeholder="상품 설명을 최대한 자세히 적어주세요."
           />
         </div>
       </div>
-
+      <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
       <div className="mb-4">
         <div className="flex items-center">
           <span className="w-48 flex-shrink-0 text-left mr-1">태그 (선택) </span>
           <input
-            className="shadow appearance-none border rounded w-8/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-8/12 py-2 px-3 text-subGray leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             placeholder="태그를 입력해주세요. (최대 5개)"
           />
@@ -113,37 +116,41 @@ const ProductReg = () => {
       <br></br>
       <br></br>
       <p className="text-left text-3xl ">추가 정보</p>
-      <hr className=" w-12/12 ml-auto mr-auto mt-10 mb-20 text-mainWhite " />
+      <hr className=" w-12/12 ml-auto mr-auto mt-6 mb-7 text-mainWhite " />
 
       <div className="mb-4">
         <div className="flex items-center">
-          <span className="w-48 flex-shrink-0 text-left mr-1">수량 </span>
+          <span className=" w-48 flex-shrink-0 text-left mr-1">수량 </span>
           <input
-            className="shadow appearance-none border rounded w-4/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+            className=" text-right shadow appearance-none border rounded w-4/12 py-2 px-3 text-subGray leading-tight focus:outline-none focus:shadow-outline "
             type="text"
-            placeholder="수량 "
+            placeholder="개 "
           />
           <span className=" text-sm text-white ml-1 mt-2">개</span>
         </div>
         </div>
+        <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
 
           {/* 거래지역 입력 */}
       <div className="mb-4">
         <div className="flex items-center">
           <span className="w-48 flex-shrink-0 text-left mr-1">거래지역</span>
+          <div className=" flex items-start flex-col">
           <input
-            className="shadow appearance-none border rounded w-2/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="text-center shadow appearance-none border rounded w-20 py-2 px-3 text-subGray leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             placeholder="내 위치"
           />
-        </div>
-        <input
-          className=" shadow appearance-none border rounded  w-4/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ml-3 mt-2 "
+          <input
+          className=" shadow appearance-none border rounded w-80 py-2 px-3 text-subGray leading-tight focus:outline-none focus:shadow-outline mt-2 "
           type="text"
           placeholder="지역 주소"
         />
+        </div>
+        </div>
+        
       </div>
-
+      <hr className=" w-12/12 ml-auto mr-auto mt-5 mb-5 border-stone-800 "/>
 
     </div>
     </form>
