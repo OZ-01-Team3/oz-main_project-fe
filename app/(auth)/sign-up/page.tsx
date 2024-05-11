f98981c8af96bc834829428d868e4a695df830cc
deswaq1220 <deswaq789@naver.com>
2024-05-12 02:08:12 +0900

    Feat: 유효성 검사 라이브러리 설치

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -35,7 +35,6 @@ const signUp = () => {
           <AuthInput type="password">Password</AuthInput>
           <AuthInput type="password">Confirm Password</AuthInput>
           <AuthInput type="text">Nickname</AuthInput>
-          <AuthInput type="text">Nickname</AuthInput>
           <AuthInput type="tel">Phone</AuthInput>
           <CommonButton className=" w-full rounded-lg bg-mainWhite px-3.5 py-2.5 text-lg font-semibold font-didot text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-6">Sign In</CommonButton>
         </form>
