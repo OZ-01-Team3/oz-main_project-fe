cd370b1dabe9faed378f80b3e65e74e6e014b393
deswaq1220 <deswaq789@naver.com>
2024-05-13 02:11:41 +0900

    Chore: 헤더 링크 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ interface MenuItem {
   label: string;
   onClick?: (event: React.MouseEvent<HTMLElement>) => void;
   Icon?: () => JSX.Element;
-  href?: string;
+  path?: string;
 }
 const Header = () => {
   const [isNotificationOpen, setIsNotificationOpen] = useState(false); // pc 상태
@@ -22,13 +22,13 @@ const Header = () => {
 
   const mainMenuItems: MenuItem[] = [
     { label: '알림', onClick: handleToggleNotification },
-    { label: '마이페이지', href: '/mypage/member-info' },
-    { label: '로그인', href: '/sign-in' },
+    { label: '마이페이지', path: '/mypage/member-info' },
+    { label: '로그인', path: '/sign-in' },
   ];
 
   const subMenuItems: MenuItem[] = [
-    { label: 'WishList', href: '/wishList' },
-    { label: 'MyCloset', href: '/404' },
+    { label: 'WishList', path: '/wishList' },
+    { label: 'MyCloset', path: '/404' },
     { label: '', Icon: () => <MagnifyingGlassIcon className="w-6" /> },
   ];
 
@@ -47,23 +47,24 @@ const Header = () => {
           {/* PC 사이즈에서만 보이도록 수정 */}
           <div className="w-auto">
             <ul className="flex justify-between w-52 text-sm font-light text-mainWhite">
-              {mainMenuItems.map(({ label, onClick, href }) => (
+              {mainMenuItems.map(({ label, onClick, path }) => (
                 <li key={label} className="cursor-pointer" onClick={onClick}>
-                  {href ? (
-                    <Link href={href}>
-                      <a>{label}</a>
-                    </Link>
+                  {path ? (
+                    <Link href={path}>{label}</Link> // 링크가 있는 경우 Link 컴포넌트 사용
                   ) : (
-                    label
+                    <span onClick={onClick}>{label}</span> // 링크가 없는 경우 일반 span 사용
                   )}
-                  {label}
                 </li>
               ))}
             </ul>
             <ul className="flex justify-between mt-3 font-didot text-mainWhite">
-              {subMenuItems.map(({ label, Icon }) => (
+              {subMenuItems.map(({ label, Icon, path }) => (
                 <li key={label || Icon?.name} className="cursor-pointer">
-                  {Icon ? <Icon /> : label}
+                  {path ? (
+                    <Link href={path}>{Icon ? <Icon /> : label}</Link> // 링크가 있는 경우 Link 컴포넌트 사용
+                  ) : (
+                    <span>{Icon ? <Icon /> : label}</span> // 링크가 없는 경우 일반 span 사용
+                  )}
                 </li>
               ))}
             </ul>
