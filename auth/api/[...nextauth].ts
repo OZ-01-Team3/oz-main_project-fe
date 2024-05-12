2c037b657fa0925b10ff09328410049b18e0ea14
deswaq1220 <deswaq789@naver.com>
2024-05-13 00:18:46 +0900

    Refactor: 로그인타입 따로 정리

---
+++ auth/api/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
