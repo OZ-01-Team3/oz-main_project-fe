384423136962116f70c8a8d90868f6eab6b92814
hayeon <hayeon010904@gmail.com>
2024-05-15 02:50:06 +0900

    feat: 사진미리보기,업로드 기능

--- app/img-reg/page.tsx
+++ app/img-reg/page.tsx
@@ -1,143 +1,134 @@
 'use client';
 import CommonButton from '@/components/CommonButton';
-import { PhotoIcon } from '@heroicons/react/16/solid';
-import { XMarkIcon } from '@heroicons/react/24/outline';
-import { ChangeEvent, useState } from 'react';
+import { PhotoIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
+import React, { ChangeEvent, useState } from 'react';
+import { toast } from 'react-toastify';
 const dragStyle = {
-  box: 'bg-[#ebe9e935] w-full h-[300px] flex flex-col items-center justify-center rounded-lg  border border-dashed border-[#FF5634] py-10 mt-5 ',
+  box: 'bg-[#ebe9e935] w-full h-[230px] flex flex-col items-center justify-center rounded-lg  border border-dashed border-[#FF5634] py-10 mt-5 mb-3 ',
   img: 'mx-auto h-60 w-6h-60 text-gray-300 text-mainBlack',
   text: 'mt-5 text-mainBlack ',
 };
 const style = {
-  box: ' w-full  h-[300px] flex flex-col items-center justify-center rounded-lg  hover:cursor-pointer border border-dashed border-mainWhite py-10 mt-5',
+  box: ' w-full  h-[230px] flex flex-col items-center justify-center rounded-lg  hover:cursor-pointer border border-dashed border-mainWhite py-10 mt-5 mb-3',
   img: 'mx-auto h-60 w-6h-60 text-gray-300   ',
   text: 'mt-5',
 };
-interface IFileTypes {
+interface FileType {
   id: number; // 파일들의 고유값 id
-  object: File;
+  file: File;
+  imageUrl: string; // 파일에 대한 미리보기 이미지 URL
 }
-const imgReg = () => {
-  // 업로드된 사진에 대한 상태
-  const [files, setFiles] = useState<IFileTypes[]>([]);
-  const [showImages, setShowImages] = useState([]);
-  //드래그앤드롭 됐는지 안됐는지 확인하기 위해 스타일 변경
-  const [isDragging, setIsDragging] = useState<boolean>(false);
-  // 구현할 InputDragDrop에서 파일이 선택될 때 상태를 업데이트 한다.
-  const handleFileSelect = (file: File | null) => {
-    setFiles(file);
-    alert('사진등록');
-  };
 
-  // 파일 업로드를 처리하는 로직
-  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const imageLists = e.target.files;
-    let imageUrlLists = [...showImages];
+const ImgReg = () => {
+  const [fileList, setFileList] = useState<FileType[]>([]); // 파일 및 이미지 URL 상태
+  const [isDragging, setIsDragging] = useState<boolean>(false); // 드래그 상태
 
-    for (let i = 0; i < imageLists.length; i++) {
-      const currentImageUrl = URL.createObjectURL(imageLists[i]);
-      imageUrlLists.push(currentImageUrl);
-    }
-
-    if (imageUrlLists.length > 5) {
-      imageUrlLists = imageUrlLists.slice(0, 5);
-    }
-
-    setShowImages(imageUrlLists);
-  };
-  // X버튼 클릭 시 이미지 삭제
-  const handleDeleteImage = id => {
-    setShowImages(showImages.filter((_, index) => index !== id));
-  };
-
-  // 사진이 박스를 진입할때
+  // 사진이 박스를 진입할 때
   const handleDragEnter = (e: React.DragEvent<HTMLLabelElement>) => {
-    e.preventDefault(); // 브라우저 이벤트 방지(이미지 넣으면 새 창 열리는거 방지해주는 역할)
-    e.stopPropagation(); // 부모 이벤트 방지
+    e.preventDefault();
+    e.stopPropagation();
     setIsDragging(true);
   };
 
-  // 사진이 박스를 벗어날때
+  // 사진이 박스를 벗어날 때
   const handleDragLeave = (e: React.DragEvent<HTMLLabelElement>) => {
     e.preventDefault();
     e.stopPropagation();
     setIsDragging(false);
   };
 
-  // 사진이 박스안에 있을 때
+  // 사진이 박스 안에 있을 때
   const handleDragOver = (e: React.DragEvent<HTMLLabelElement>) => {
     e.preventDefault();
     e.stopPropagation();
     setIsDragging(true);
   };
 
-  // 사진을 박스에서 드롭할때
   const handleDrop = (e: React.DragEvent<HTMLLabelElement>) => {
     e.preventDefault();
     e.stopPropagation();
     setIsDragging(false);
-    if (e.dataTransfer) {
-      const file = e.dataTransfer.files[0];
-      handleFileSelect(file);
+
+    const newFileList: FileType[] = [...fileList];
+    const files = e.dataTransfer.files;
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const imageUrl = URL.createObjectURL(file);
+      newFileList.push({ id: fileList.length + i, file, imageUrl });
     }
+
+    if (newFileList.length > 5) {
+      newFileList.splice(5); // 최대 5장까지 제한
+      toast.error('사진은 최대 다섯 장까지 등록할 수 있습니다.');
+    }
+
+    setFileList(newFileList);
   };
 
-  // Drag & Drop이 아닌 클릭 이벤트로 업로드 기능
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
-    handleFileSelect(file);
-    // input 요소의 값 초기화
-    e.target.value = '';
-    console.log(file);
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setFileList([...fileList, { id: fileList.length, file, imageUrl }]);
+    }
+    e.target.value = ''; // input 값 초기화
   };
 
+  const handleDeleteImage = (id: number) => {
+    const filteredList = fileList.filter(item => item.id !== id);
+    setFileList(filteredList);
+  };
+  const handleTest = () => {
+    console.log(fileList);
+  };
   return (
-    <>
-      <div className="w-[600px] md:w-[400px] sm:w-[400px]  m-auto flex flex-col justify-center items-center mt-10 ">
-        <div className="w-full h-full flex flex-col justify-center items-center">
-          <p className="">상품 이미지 등록</p>
-          <label
-            className={isDragging ? dragStyle.box : style.box}
-            onDragEnter={handleDragEnter}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-            onDragOver={handleDragOver}
-          >
-            <input
-              type="file"
-              id="fileUpload"
-              style={{ display: 'none' }}
-              multiple={true} // 파일 다중선택 허용
-              onChange={handleUpload}
-            />
-
-            <PhotoIcon className={isDragging ? dragStyle.img : style.img} aria-hidden="true" />
-            <p className={isDragging ? dragStyle.text : style.text}>클릭하거나 파일을 여기에 드롭하여 선택하세요</p>
-          </label>
-          <div className=" w-full h-[100px] flex justify-center items-center">
-            {showImages.map((image, id) => (
-              // 히나의 사진
-              <div key={id} className=" w-20 h-20 flex mr-3 relative">
-                <div className="w-20 h-20 border bg-mainWhite">
-                  <img src={image} alt={`${image}-${id}`} className="object-contain w-full h-full" />
-                </div>
-
-                <XMarkIcon
-                  className="w-3 h-3 absolute right-1 top-1 cursor-pointer text-mainBlack"
-                  onClick={() => handleDeleteImage(id)}
-                />
+    <div className="w-[600px] md:w-[400px] sm:w-[400px] mx-auto flex flex-col justify-between items-center h-[calc(100vh-95.99px)]">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <p className="">상품 이미지 등록</p>
+        <label
+          className={isDragging ? dragStyle.box : style.box}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+        >
+          <input
+            type="file"
+            id="fileUpload"
+            style={{ display: 'none' }}
+            multiple // 파일 다중 선택 허용
+            onChange={handleChange}
+          />
+          <PhotoIcon className={isDragging ? dragStyle.img : style.img} aria-hidden="true" />
+          <p className={isDragging ? dragStyle.text : style.text}>
+            클릭하거나 파일을 여기에 드롭하여 선택하세요(최대 5장)
+          </p>
+        </label>
+        {/* 사진 미리보기 박스 */}
+        <div className="w-full h-[100px] flex justify-center items-center">
+          {fileList.map(item => (
+            <div key={item.id} className="w-20 h-20 flex mr-3 relative">
+              <div className="w-20 h-20 border bg-mainWhite">
+                <img src={item.imageUrl} alt={`preview-${item.id}`} className="object-contain w-full h-full" />
               </div>
-            ))}{' '}
-          </div>
+              <XMarkIcon
+                className="w-3 h-3 absolute right-1 top-1 cursor-pointer text-mainBlack"
+                onClick={() => handleDeleteImage(item.id)}
+              />
+            </div>
+          ))}
         </div>
-
-        <CommonButton className=" w-80 rounded-lg bg-mainWhite mt-5 py-3 font-semibold text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ">
+        <CommonButton
+          onClick={handleTest}
+          className="w-80 rounded-lg mt-10 bg-mainWhite py-3 font-semibold text-mainBlack shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+        >
           다음
         </CommonButton>
       </div>
-    </>
+    </div>
   );
 };
 
-export default imgReg;
+export default ImgReg;
