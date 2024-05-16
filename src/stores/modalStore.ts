e539d993a126ab4e707dd08c9283cae52752ecf6
hayeon <hayeon010904@gmail.com>
2024-05-17 00:13:39 +0900

    feat: 상품 아이디 저장하는 상태,모달 열고닫는 상태 zustand 생성

---
+++ src/stores/modalStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+
+// const [selectedProductId, setSelectedProductId] = useState(0);
+// 상품 아이디 저장하는 상태
+interface ProductId {
+  selectedProductId: number;
+  setSelectedProductId: (id: number) => void;
+}
+export const useProductIdStore = create<ProductId>(set => ({
+  selectedProductId: 0,
+  setSelectedProductId: id => set({ selectedProductId: id }),
+}));
+
+// 모달 열고닫는 함수
+// const [detailModalOpen, setDetailModalOpen] = useState(false);
+interface ModalOpenStore {
+  detailModalOpen: boolean;
+  setDetailModalOpen: (prev: boolean) => void;
+}
+
+export const useModalOpenStore = create<ModalOpenStore>(set => ({
+  detailModalOpen: false,
+  setDetailModalOpen: () => set(state => ({ detailModalOpen: !state.detailModalOpen })),
+}));
