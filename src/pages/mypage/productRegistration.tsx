f2822d2606749f15fe555a17f63f2e69cc8ef722
hayeon <hayeon010904@gmail.com>
2024-05-22 02:26:11 +0900

    feat: 상품등록 기능 성공

--- src/pages/mypage/productRegistration.tsx
+++ src/pages/mypage/productRegistration.tsx
@@ -1,15 +1,15 @@
-import instance from '@/api/instance';
 import { productRequests } from '@/api/productRequest';
 import CommonButton from '@/components/CommonButton';
 import axios from 'axios';
 import { ChangeEventHandler, useEffect, useState } from 'react';
+import { Cookies } from 'react-cookie';
 import { FormProvider, useForm } from 'react-hook-form';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 const { VITE_BASE_REQUEST_URL } = import.meta.env;
 const sizes = ['S', 'M', 'L', 'XL'];
-// const hardCategories = ['전체', '아우터', '상의', '하의', '잡화', '신발'];
-// const setCategories = [0, 1, 2, 3, 4, 5];
+const cookies = new Cookies();
+
 export const productStatusOptions = [
   { id: 1, label: '새 상품 (미사용)', description: '사용하지 않은 상품' },
   { id: 2, label: '사용감 없음', description: '사용은 했지만 눈에 띄는 흔적이나 얼룩이 없음' },
@@ -29,8 +29,8 @@ interface category {
 
 // const productRegistrationSchema = zod.object({
 //   name: zod.string().min(1, '상품명을 입력해주세요').max(30, '상품명은 30자 이내로 입력해주세요'),
-//   purchasing_price: zod.string().min(1, '구매가를 입력해주세요'),
-//   rental_fee: zod.string().min(1, '대여비를 입력해주세요'),
+//   purchasing_price: zod.number(),
+//   rental_fee: zod.number(),
 //   size: zod.string().min(1, '사이즈를 선택해주세요'),
 //   brand: zod.string().min(1, '브랜드를 입력해주세요'),
 //   product_category: zod.string().min(1, '카테고리를 선택해주세요'),
@@ -56,17 +56,15 @@ const ProductRegistration = () => {
       toast.error('사진을 등록해주세요!');
       return;
     }
-
     handleGetCategories();
   }, []);
 
   //**받아온 사진 배열에서 사진파일배열만 보내기 */
-  const registeredImages = (location.state as locationState[]).map((item, idx) => {
-    console.log(`index: ${idx}`);
-    console.log(item.file);
+  const registeredImages = (location.state as locationState[]).map(item => {
     return item.file;
   });
   console.log(registeredImages);
+
   const handleProductNameMaxLength: ChangeEventHandler<HTMLInputElement> = e => {
     const value = e.target.value;
     if (value.length <= 30) {
@@ -93,9 +91,9 @@ const ProductRegistration = () => {
       // tag: '',
       amount: '',
       region: '',
-      images: '',
+      image: '',
     },
-    mode: 'onChange',
+    mode: 'onSubmit',
   });
   const {
     register,
@@ -114,13 +112,7 @@ const ProductRegistration = () => {
     }
   };
 
-  //? 이렇게 하나하나 다 append 해줘야만하는건가 ...
   const handleClickReg = handleSubmit(async data => {
-    // if (Object.keys(errors).length > 0) {
-    //   alert('모든 필수 항목을 입력해주세요!');
-    //   return;
-    // }
-
     try {
       const formData = new FormData();
       formData.append('name', data.name);
@@ -136,7 +128,7 @@ const ProductRegistration = () => {
       formData.append('region', data.region);
 
       // registeredImages 파일을 formData에 추가
-      registeredImages.forEach((image, index) => {
+      registeredImages.forEach(image => {
         formData.append(`image`, image);
         console.log(image);
       });
@@ -144,73 +136,79 @@ const ProductRegistration = () => {
       for (const pair of formData.entries()) {
         console.log(`${pair[0]}: ${pair[1]}`);
       }
-      const response = await instance.post(productRequests.products, formData);
-
+      const access = cookies.get('ac');
+      const response = await axios.post(VITE_BASE_REQUEST_URL + productRequests.products, formData, {
+        headers: {
+          Authorization: `Bearer ${access}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      });
       console.log('상품 등록 성공', response);
+      toast.success('상품이 성공적으로 등록되었습니다!');
+      navigate('/');
     } catch (error) {
       console.log(error);
+      toast.error('상품등록에 실패하였습니다');
     }
     console.log(data);
   });
   return (
     <div className="lg:w-[700px] w-[900px] md:w-[500px] sm:w-[350px] sm:text-sm m-auto ">
       <FormProvider {...form}>
-        <form className="w-full" onSubmit={handleClickReg}>
+        <form className="w-full md:mb-20 sm:mb-20" onSubmit={handleClickReg}>
           <p className="text-left text-3xl mt-28">상품 정보</p>
+
           <hr className="w-full ml-auto mr-auto mt-6 mb-7 text-hrGray" />
 
           {/* Input fields */}
-          <div>
-            {/* 상품명 */}
-            {/* //? 이거 Input 값이 안들어와... */}
-            {/* <TextField type="text" label="상품명" placeholder="상품명을 입력하세요" />
-            <TextField
-              type="number"
-              label="구매가"
-              placeholder="상품을 구매하신 금액을 입력하세요"
-              {...register('purchasing_price')}
-            />
-            <TextField
-              {...register('name')}
-              type="number"
-              label="대여비"
-              placeholder="대여비를 입력하세요"
-              {...register('rental_fee')}
-            /> */}
-            <div className="mb-4">
-              <div className="flex items-center justify-center w-full">
-                <span className="w-1/4 text-left flex-shrink-0 mr-1 pl-5">상품명</span>
-                <input
-                  type="text"
-                  className="shadow appearance-none border rounded w-full md:w-8/12 py-2 px-3 text-mainBlack leading-tight focus:outline-none focus:shadow-outline placeholder-subGray focus:border-mainWhite focus:bg-mainWhite"
-                  placeholder="상품명을 입력하세요"
-                  {...register('name')}
-                  onChange={handleProductNameMaxLength}
-                />
-                <span className="text-sm text-white ml-3">{productNameLength}/30</span>
+          {/* <div> */}
+          {/* 상품명 */}
+          <div className="mb-4 text-center">
+            <div className="flex items-center justify-center w-full">
+              <span className="w-1/4 text-left flex-shrink-0 mr-1 pl-5">상품명</span>
+              <div className=" w-full md:w-8/12 flex flex-col justify-start items-center">
+                <div className="w-full flex justify-between items-center ">
+                  <input
+                    type="text"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-mainBlack leading-tight focus:outline-none focus:shadow-outline placeholder-subGray focus:border-mainWhite focus:bg-mainWhite"
+                    placeholder="상품명을 입력하세요"
+                    {...register('name')}
+                    onChange={handleProductNameMaxLength}
+                  />
+                  <span className="text-sm text-white ml-3">{productNameLength}/30</span>
+                </div>
+                {errors.name && <p className=" text-sm text-red-500 mt-1 w-full text-left">{errors.name.message}</p>}
               </div>
-              <hr className="w-full ml-auto mr-auto mt-5 mb-5 border-stone-800" />
             </div>
-            {/* 구매가 */}
-            <div className="mb-4">
-              <div className="flex items-center justify-center w-full">
-                <span className="w-1/4 text-left flex-shrink-0 mr-1 pl-5">구매가</span>
-                <input
-                  type="text"
-                  className="shadow appearance-none border rounded w-full md:w-8/12 py-2 px-3 text-mainBlack leading-tight focus:outline-none focus:shadow-outline placeholder-subGray focus:border-mainWhite focus:bg-mainWhite"
-                  placeholder="상품을 구매하신 금액을 입력하세요"
-                  {...register('purchase_price')}
-                />
-                <span className="text-sm text-white ml-3">원</span>
+
+            <hr className="w-full ml-auto mr-auto mt-5 mb-5 border-stone-800" />
+          </div>
+          {/* 구매가 */}
+          <div className="mb-4 text-center">
+            <div className="flex items-center justify-center w-full">
+              <span className="w-1/4 text-left flex-shrink-0 mr-1 pl-5">구매가</span>
+              <div className=" w-full md:w-8/12 flex flex-col justify-start items-center">
+                <div className="w-full flex justify-between items-center ">
+                  <input
+                    type="number"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-mainBlack leading-tight focus:outline-none focus:shadow-outline placeholder-subGray focus:border-mainWhite focus:bg-mainWhite"
+                    placeholder="상품을 구매하신 금액을 입력하세요"
+                    {...register('purchase_price')}
+                  />
+                  <span className="text-sm text-white ml-3">원</span>
+                </div>
+                {errors.purchase_price && (
+                  <p className=" text-sm text-red-500 mt-1 w-full text-left">{errors.purchase_price.message}</p>
+                )}
               </div>
-              <hr className="w-full ml-auto mr-auto mt-5 mb-5 border-stone-800" />
             </div>
+            <hr className="w-full ml-auto mr-auto mt-5 mb-5 border-stone-800" />
             {/* 대여비 */}
             <div className="mb-4">
               <div className="flex items-center justify-center w-full">
                 <span className="w-1/4 text-left flex-shrink-0 mr-1 pl-5">대여비</span>
                 <input
-                  type="text"
+                  type="number"
                   className="shadow appearance-none border rounded w-full md:w-8/12 py-2 px-3 text-mainBlack leading-tight focus:outline-none focus:shadow-outline placeholder-subGray focus:border-mainWhite focus:bg-mainWhite"
                   placeholder="대여비를 입력하세요"
                   {...register('rental_fee')}
