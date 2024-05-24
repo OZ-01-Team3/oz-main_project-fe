b84dccd96cd9c65d2309ecf92d4ddbb782348c8d
hayeon <hayeon010904@gmail.com>
2024-05-25 01:51:32 +0900

    feat: 알림기능구현(알림읽음기능제외), 기타 오류 수정,콘솔 몇개 삭제

---
+++ src/NotificationContext.tsx
@@ -0,0 +1,66 @@
+import { PropsWithChildren, createContext, useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import useNotificationStore from './stores/useNotification';
+
+interface NotificationContextType {
+  webSocketState: WebSocket | null;
+  sendCommand: (command: string, notification_id: number) => void;
+}
+
+export const notificationContext = createContext<NotificationContextType | null>(null);
+
+export const useNotificationContext = () => {
+  return useContext(notificationContext);
+};
+
+export const NotificationProvider = ({ children }: PropsWithChildren) => {
+  const [webSocketState, setWebSocketState] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const webSocket = new WebSocket(`ws://127.0.0.1:8000/ws/notification/`);
+    webSocket.onopen = () => {
+      console.log('웹소켓 연결.');
+    };
+
+    webSocket.onclose = event => {
+      console.log('웹소켓 연결 종료.', event);
+    };
+
+    webSocket.onerror = error => {
+      console.log('웹 소켓 에러.', error);
+    };
+
+    webSocket.onmessage = event => {
+      const data = JSON.parse(event.data);
+      toast.info('새로운 알림이 있습니다');
+
+      if (data.type) {
+        useNotificationStore.getState().addNotification(data);
+      } else {
+        useNotificationStore.getState().addListNotification(data);
+      }
+
+      console.log('새 알림.', data);
+    };
+
+    setWebSocketState(webSocket);
+
+    return () => {
+      webSocket.close();
+    };
+  }, []);
+  const sendCommand = (command: any, notification_id: number) => {
+    if (webSocketState && webSocketState.readyState === WebSocket.OPEN) {
+      webSocketState.send(
+        JSON.stringify({
+          command: command,
+          notification_id: notification_id,
+        })
+      );
+    }
+  };
+
+  return (
+    <notificationContext.Provider value={{ webSocketState, sendCommand }}>{children}</notificationContext.Provider>
+  );
+};
