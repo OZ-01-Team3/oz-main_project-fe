a61ac7e2fbac42efdded00bf0ad1221507d1e48a
deswaq1220 <deswaq789@naver.com>
2024-05-28 16:47:19 +0900

    Chore:뒷배경 컬러변경

--- src/components/chat/ChatDeleteModal.tsx
+++ src/components/chat/ChatDeleteModal.tsx
@@ -60,7 +60,7 @@ const ChatDeleteModal = ({ setOpen, deleteChatRoom }: ChatDeleteModalProps) => {
                 <div className="mt-5 sm:mt-6 grid grid-flow-row-dense grid-cols-2 gap-x-3 ">
                   <button
                     type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-shadow-sm ring-1 ring-inset ring-gray-300 text-zinc-900 hover:bg-gray-50"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-shadow-sm ring-1 ring-inset ring-gray-300 text-mainBlack hover:bg-gray-50"
                     onClick={handleClose}
                     ref={cancelButtonRef}
                   >
