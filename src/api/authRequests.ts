aecc2ad2b6d7f43aad7da03f1aad1c6a11f7389f
deswaq1220 <deswaq789@naver.com>
2024-05-28 16:45:43 +0900

    Chore: 회원수정타입 추가, 토큰리프레시 주석 삭제, 회원정보수정 api

--- src/api/authRequests.ts
+++ src/api/authRequests.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import instance from './instance';
 
 const { VITE_BASE_REQUEST_URL } = import.meta.env;
 
@@ -15,6 +16,13 @@ interface SignUpType {
   phone: string;
 }
 
+interface modificationUserInfoType {
+  password1: string | undefined;
+  password2: string | undefined;
+  nickname: string | undefined;
+  phone: string | undefined;
+}
+
 // interface TokenRefreshResponse {
 //   refresh: string;
 // }
@@ -23,6 +31,8 @@ const authRequests = {
   signUp: '/users/signup/', // 회원가입
   login: '/users/login/', //로그인
   logout: '/users/logout/', // 로그아웃
+
+  withdrawal: '/users/leave/', //회원탈퇴
   userInfo: '/users/detail/', // 회원 정보조회, 수정, 일부수정
   // 토큰
   userLeave: '/users/token/verify/', //access token 유효성 검사
@@ -31,45 +41,38 @@ const authRequests = {
   sendCode: '/users/send-code/', // 이메일 코드 전송
   verifyEmail: '/users/confirm-email/', // 이메일 인증
 };
-
-//로그인
+/** 로그인 */
 export const loginAPI = (email: string, password: string) =>
   axiosClient.post<UserActivation>(authRequests.login, {
     email,
     password,
   });
 
-//회원가입
+/** 로그아웃 */
+export const logoutAPI = () => instance.post(authRequests.logout);
+
+/** 회원탈퇴 */
+export const withdrawalAPI = () => instance.delete(authRequests.withdrawal);
+
+/** 회원가입 */
 export const signUpAPI = (data: SignUpType): Promise<AxiosResponse<UserActivation, any>> =>
   axiosClient.post<UserActivation>(authRequests.signUp, data);
 
-//이메일 전송
+/** 이메일 전송 */
 export const sendCodeAPI = (email: string) =>
   axiosClient.post<UserActivation>(authRequests.sendCode, {
     email,
   });
 
-// 이메일 인증
+/** 이메일 인증 */
 export const verifyEmailAPI = (email: string, code: string) => {
   axiosClient.post<UserActivation>(authRequests.verifyEmail, {
     email,
     code,
   });
 };
-
-// 토큰 리프레시
-// export const refreshAPI = (): Promise<AxiosResponse<TokenRefreshResponse>> => {
-//   const refreshToken = cookies.get('rc');
-//   return axios.post<TokenRefreshResponse>(
-//     authRequests.refresh,
-//     {},
-//     {
-//       headers: {
-//         Authorization: `Bearer ${refreshToken}`,
-//         'Content-Type': 'application/json',
-//       },
-//     }
-//   );
-// };
+/**회원 정보 수정 */
+export const modificationUserInfoAPI = (data: modificationUserInfoType): Promise<AxiosResponse<UserActivation, any>> =>
+  instance.patch<UserActivation>(authRequests.userInfo, data);
 
 export default authRequests;
