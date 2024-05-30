d60cfb45a85b149cee951bd0e1b4b8a216f2d8a1
deswaq1220 <deswaq789@naver.com>
2024-05-30 18:57:53 +0900

    Fix: 링크 연결

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,5 +1,7 @@
 
 import { HeartIcon, HomeIcon, MagnifyingGlassIcon, Squares2X2Icon, UserCircleIcon } from "@heroicons/react/24/outline"
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
 
 
 // interface ChatNotificationProps {
@@ -10,6 +12,7 @@ import { HeartIcon, HomeIcon, MagnifyingGlassIcon, Squares2X2Icon, UserCircleIco
 
 const MobileNave = () => {
 
+  const navigate = useNavigate()
   // // 헤더에서 알림버튼 눌렀을때 뒤에꺼 스크롤 안되게 
   // useEffect(() => {
   //   if (isOpen) {
@@ -24,23 +27,23 @@ const MobileNave = () => {
     <div className="relative w-full">
       <div className="fixed bottom-0 right-0 z-50 w-full bg-white px-4 py-1.5 overflow-auto">
         <div className="flex justify-between">
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center cursor-pointer" onClick={() => navigate('/')}>
             <HomeIcon className=" w-8 h-8 text-mainBlack" />
             <p className="text-subGray text-xs font-thin">Home</p>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center cursor-pointer" onClick={() => navigate('/wish-list')}>
             <HeartIcon className="w-8 h-8 text-mainBlack" />
             <p className="text-mainBlack text-xs font-thin">Whish list</p>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center cursor-pointer" onClick={() => navigate('/search')}>
             <MagnifyingGlassIcon className="w-8 h-8 text-mainBlack" />
             <p className="text-mainBlack text-xs font-thin">Search</p>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center cursor-pointer" onClick={() => toast.info("준비중입니다")}>
             <Squares2X2Icon className="w-8 h-8 text-mainBlack" />
             <p className="text-mainBlack text-xs font-thin">My closet</p>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center cursor-pointer" onClick={() => navigate('/mypage/member-info')}>
             <UserCircleIcon className="w-8 h-8 text-mainBlack" />
             <p className="text-mainBlack text-xs font-thin">My page</p>
           </div>
